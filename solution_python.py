ec68a40a42bde0760045a485d3e27149febe13c0
ThomasJuric <thomasjuric@gmail.com>
2020-07-10 14:48:53 -0400

    Added comments

--- solution_python.py
+++ solution_python.py
@@ -1,3 +1,4 @@
+#Create lists, one for main list and one to keep track of what has been popped off
 list = []
 undoList = []
 
@@ -7,15 +8,19 @@ class EventSourcer():
         self.value = 0
 
     def add(self, num: int):
+        # Add the value to the overall, and add int to list
         self.value = self.value + num
         list.append(num)
     
     def subtract(self, num: int):
+        # Subtract the value from the overall value, and add the negative number to the list
         self.value = self.value + (-1 * num)
         list.append((-1 * num))
 
     def undo(self):
+        # Check if something has been sent as a parameter
         if(self.value != 0):
+            # Pop the first object from the list, and subtract it from the overall value
             size = len(list)
             undoList.append(list[size-1])
             self.value = self.value - list.pop()
@@ -23,15 +28,17 @@ class EventSourcer():
             pass
         
     def redo(self):
+        # Check if there is a parameter
         if(self.value != 0):
+            # Add the last item from the undoList onto the original list, as it was the last item popped off
             self.value = self.value + undoList.pop()
         else:
             pass
-        
+    # Loop through
     def bulk_undo(self, steps: int):
         for x in range(steps):
-            self.undo()
-
+                self.undo()
+    # Loop through previous functions
     def bulk_redo(self, steps: int):
         for x in range(steps):
             self.redo()
