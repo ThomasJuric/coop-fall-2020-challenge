da9547c2bc427198df434ae8d58b901757ef8a23
ThomasJuric <thomasjuric@gmail.com>
2020-07-10 14:35:07 -0400

    Fixed undo and bulk_undo

--- solution_python.py
+++ solution_python.py
@@ -1,24 +1,29 @@
+list = []
+
 class EventSourcer():
     # Do not change the signature of any functions
-
     def __init__(self):
         self.value = 0
 
     def add(self, num: int):
         self.value = self.value + num
+        list.append(num)
     
     def subtract(self, num: int):
-        self.value = self.value - num
-        
+        self.value = self.value + (-1 * num)
+        list.append(-1 * num)
 
     def undo(self):
-        pass
-
+        self.value = self.value - list.pop()
+        
     def redo(self):
         pass
 
     def bulk_undo(self, steps: int):
-        pass
+        for x in range(steps):
+            self.undo()
 
     def bulk_redo(self, steps: int):
         pass
+
+    
